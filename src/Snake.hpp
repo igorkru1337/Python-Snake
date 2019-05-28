5270bc7280736b73d71227bd49d80ed83342cbb5
Igor <krupenni@list.ru>
2019-05-28 18:39:07 +0700

    Update after refactoring code

--- src/Snake.hpp
+++ src/Snake.hpp
@@ -7,17 +7,12 @@
 #include <time.h>
 
 using namespace sf;
-int N = 39, M = 39;
-int scale = 25;
-
-int w = scale * N;
-int h = scale * M;
-
-int dir, num = 4;
+extern int dir;
 int game(RenderWindow& window);
 void Display(sf::RenderWindow& win, Texture& GamegroundTexture, Texture& FructTexture, Texture& HeadLeftTexture);
 void Key(int& dir);
 void Snake(sf::RenderWindow& win, Texture& HeadLeftTexture);
 void Draw(sf::RenderWindow& win, Texture& GamegroundTexture);
-
+int RotateHead(int dir, Sprite& head);
+int SnakeCrush(struct S* s);
 #endif
